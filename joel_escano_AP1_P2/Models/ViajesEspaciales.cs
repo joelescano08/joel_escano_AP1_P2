8decf370497523cd2fa842fca023650c67bb2491
Jeg <joelescanoguzman@gmail.com>
2026-03-02 18:53:28 -0400

    Crea modelo base de Viajes Espaciales

---
+++ joel_escano_AP1_P2/Models/ViajesEspaciales.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace joel_escano_AP1_P2.Models
+{
+    public class ViajesEspaciales
+    {
+
+        [Key]
+        public int ViajeId { get; set; }
+
+        public DateTime FechaIda { get; set; } = DateTime.Now;
+        public DateTime FechaRegreso { get; set; };
+
+
+    }
+}
