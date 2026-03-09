b471547467bb4c0c4aaeae9a2b83f0f8860b9662
Jeg <joelescanoguzman@gmail.com>
2026-03-09 18:24:23 -0400

    Crea modelo de Estudiantes

---
+++ joel_escano_AP1_P2/Models/Estudiantes.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace joel_escano_AP1_P2.Models;
+
+public class Estudiantes
+{
+    [Key]
+
+    public int EstudianteId { get; set; }
+
+    [Required(ErrorMessage = "Este Campo Es Requerido")]
+
+    public string Nombres { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Este Campo Es Requerido")]
+
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Este Campo Es Requerido")]
+
+    public int Edad { get; set; }
+
+    public int BalancePuntos { get; set; } = 0;
+}
