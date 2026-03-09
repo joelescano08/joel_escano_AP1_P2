9dca703b01ffa3489f1bc302a5209721dd5407dd
Jeg <joelescanoguzman@gmail.com>
2026-03-09 18:33:26 -0400

    Crea modelo para Detalle de AsignacionesPuntos

---
+++ joel_escano_AP1_P2/Models/AsignacionPuntosDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace joel_escano_AP1_P2.Models;
+
+public class AsignacionPuntosDetalle
+{
+    [Key]
+    public int IdDetalle { get; set; }
+
+    public int IdAsignacion { get; set; }
+
+    public int TipoPuntosId { get; set; }
+
+    public int CantidadPuntos { get; set; }
+
+}
