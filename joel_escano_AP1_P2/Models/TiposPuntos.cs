572cda4c2df7f40eaf0c8d172e8b78819483037a
Jeg <joelescanoguzman@gmail.com>
2026-03-09 18:25:40 -0400

    Crea modelo TiposPuntos

---
+++ joel_escano_AP1_P2/Models/TiposPuntos.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace joel_escano_AP1_P2.Models;
+
+public class TiposPuntos
+{
+    [Key]
+
+    public int TipoId { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Este Campo Es Requerido")]
+
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Este Campo Es Requerido")]
+
+    public int ValorPuntos { get; set; }
+
+    [Required(ErrorMessage = "Este Campo Es Requerido")]
+
+    public string Color { get; set; }
+
+    [Required(ErrorMessage = "Este Campo Es Requerido")]
+
+    public string Icono { get; set; }
+
+    [Required(ErrorMessage = "Este Campo Es Requerido")]
+
+    public bool Activo { get; set; } = true;
+}
