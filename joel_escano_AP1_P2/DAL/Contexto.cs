71d59ca5c34df3dbfff85eefe7828735c9e4c2e7
Jeg <joelescanoguzman@gmail.com>
2026-03-09 19:05:47 -0400

    Agrega el DbSet de AsignacionesPuntos en el contexto

--- joel_escano_AP1_P2/DAL/Contexto.cs
+++ joel_escano_AP1_P2/DAL/Contexto.cs
@@ -6,6 +6,8 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options)  : base(options) {}
 
+    public DbSet<AsignacionesPuntos> AsignacionesPuntos { get; set; }
+    public DbSet<AsignacionesPuntosDetalle> AsignacionesPuntosDetalle { get; set; }
     public DbSet<Estudiantes> Estudiantes { get; set; }
 
     public DbSet<TiposPuntos> TiposPuntos { get; set; }
